930fcdaa4bf3210e8cf5f255464059d437cbec72
gseok89 <73097891+gseok89@users.noreply.github.com>
2023-10-05 01:12:30 +0900

    질답 페이지 완료, 로딩/결과 화면 진행하기

--- src/Travel.js
+++ src/Travel.js
@@ -3,6 +3,7 @@ import './Travel.css';
 
 function App() {
 
+  
   const setVh = () => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`)
@@ -25,44 +26,44 @@ function App() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const questionList = [
     {q:['포토존 앞에서 어떤 커플이\n사진을 찍어달라고 한다.'],
-    a:[{type:'I',text:'당연하죠! 남자분 고개 이쪽으로 더 땡겨주세요~\n(촬영후) 저희도 찍어주세요! ^o^'},
-       {type:'E',text:'어쩔 수 없지 뭐ㅠㅠ (다른사람 누구 만나지?)'}]},
-    {q:['너 이번주에 엄청 바빴다며','주말엔 뭐해?'],
-    a:[{type:'I',text:'너무 힘들었어ㅠㅠ 집에서 쉬어야지'},
-       {type:'E',text:'바빠서 못 놀았어ㅠㅠ 나가 놀아야지'}]},
-    {q:['(자주 가는 카페 사장님이 아는 척을 했다)'],
-    a:[{type:'I',text:'(이제 그만 와야지)'},
-       {type:'E',text:'(더 자주 와야지)'}]},
+    a:[{type:'I',text:'네~ (최대한 정성스럽게 여러번 찍는다)\n좋은 하루 되세요!'},
+       {type:'E',text:'당연하죠! 남자분 고개 이쪽으로 더 땡겨주세요~\n(촬영후) 저희도 찍어주세요! ^o^'}]},
+    {q:['축제장에 사람이 많아\n활기차고 시끌벅적하다.'],
+    a:[{type:'I',text:'재밌겠다ㅎㅎ\n(이렇게 많을 줄 몰랐는데...)'},
+       {type:'E',text:'와 진짜 재밌는거 많은가 보다ㅋㅋ\n얼른 구경하러 가자!'}]},
+    {q:['축제를 재밌게 즐기고\n집에 도착했다.'],
+    a:[{type:'I',text:'응응 오늘 재밌었어 ㅎㅎ 일단 좀 쉬구 생각해보자!'},
+       {type:'E',text:'다른 축제도 진짜 재밌을 것 같은데 거기도 가보자 ^~^'}]},
 
-    {q:['넌 노래 들을 때, 뭘 중요하게 생각해?'],
-    a:[{type:'S',text:'멜로디'},
-       {type:'N',text:'가사'}]},
-    {q:['사과하면 뭐가 떠올라?'],
-    a:[{type:'S',text:'빨갛다. 맛있다. 동그랗다?'},
-       {type:'N',text:'아이폰 로고ㅋㅋ 백설공주도 생각난다.'}]},
-    {q:['오늘 점심 뭐 먹을래?'],
-    a:[{type:'S',text:'음. 파스타 먹을까?'},
-       {type:'N',text:'파스타 먹을까? 아! 파스타 먹으면 느끼하니까 저녁엔 김치찌개 먹어야겠다!'}]},
+    {q:['자기야! 화순에서\n가을꽃 축제 한다는데 갈래?'],
+    a:[{type:'S',text:'좋아! 가을꽃 축제에 이쁜 거 많겠다.'},
+       {type:'N',text:'가을꽃 축제? 가을에만 피는 꽃을 말하는 건가?\n가을꽃 종류가 따로 있는 건가? 헷갈리네...'}]},
+    {q:['국화모양의 헬륨 풍선이 있다.\n떠오르는 생각은?'],
+    a:[{type:'S',text:'우와 진짜 이쁘다! 나 저거 사줘!'},
+       {type:'N',text:'헬륨풍선으로 꽃다발 만들면 날아가려나?'}]},
+    {q:['축제에 오는데 내비가\n내가 아는 길로 알려주질 않는다.'],
+    a:[{type:'S',text:'음 내비가 정확하겠지^^ 가라는대로 가보자'},
+       {type:'N',text:'내가 알고 있는 길이 더 빠를 것 같은데?'}]},
 
-    {q:['나 요즘 너무 우울해서','여행 가려고'],
-    a:[{type:'F',text:'무슨 일 있어?'},
-       {type:'T',text:'어디로 여행가게?'}]},
-    {q:['슬픔을 나누면 어떻게 될까?'],
-    a:[{type:'F',text:'슬픔이 반이 되지'},
-       {type:'T',text:'슬픈 사람이 둘이 되겠지'}]},
-    {q:['나 시험에서 떨어졌어ㅠㅠ'],
-    a:[{type:'F',text:'많이 속상하겠다...ㅠㅠ'},
-       {type:'T',text:'무슨 시험 봤는데? 몇 점?'}]},
+    {q:['축제장에 있는 수 많은 꽃들과\n조형물을 보면서 드는 생각은?'],
+    a:[{type:'F',text:'와 진짜 이쁘다. 자기야 나 여기서 찍어줘!'},
+       {type:'T',text:'와 진짜 대단하다. 이건 어떻게 만들었을까?'}]},
+    {q:['사고 싶었지만 비싸서 안 샀던\n기념품을 애인이 몰래 사왔다.'],
+    a:[{type:'F',text:'그거 비쌌을 텐데ㅠㅠ 고마워'},
+       {type:'T',text:'고마워. 그거 진짜 갖고 싶은 거였어'}]},
+    {q:['조금 전에 기념품을 샀는데\n애인이 또 사려고 한다.'],
+    a:[{type:'F',text:'그래! 이쁜건 사야지~'},
+       {type:'T',text:'그거 결국 쓰레기 된다..?'}]},
 
-    {q:['안 읽은 메시지 갯수 몇 개야?'],
-    a:[{type:'P',text:'10개 이상'},
-       {type:'J',text:'0개 ~ 한자리 수'}]},
-    {q:['여행 일정 짰어?'],
-    a:[{type:'P',text:'ㅇㅇ 국밥먹고 바다가서 놀다가 카페 가자'},
-       {type:'J',text:'7시30분 만남, 8시 할매국밥, 9시 유리박물관, 11시 유리해수욕장, 12시 유리카페...'}]},
-    {q:['2주 뒤에 시험이다.'],
-    a:[{type:'P',text:'시험이 2주나 남았네!'},
-       {type:'J',text:'시험이 2주밖에 안 남았네.'}]},
+    {q:['3일 뒤면 가을꽃 축제각.'],
+    a:[{type:'P',text:'축제장 가면 알아서 재밌는 거 많이 있겠지~'},
+       {type:'J',text:'뭐뭐 있는지 알아봐야지'}]},
+    {q:['축제에서 데이트할 때 비용은?'],
+    a:[{type:'P',text:'오늘 얼마 썼더라..? 음 대충 3만원^^ (5만원 넘게 씀)'},
+       {type:'J',text:'이거 먹구 저거 사구.. 오늘은 텅장 안 되게 3만원만 써야겠다!'}]},
+    {q:['축제에서 예쁜 기념품을 사서 온 후,'],
+    a:[{type:'P',text:'일단 누워서 쉰다. 다음날 나갈 때 기념품 발견. 여기 둬야겠다!'},
+       {type:'J',text:'내가 생각했던 장소에 기념품 안착!'}]},
 
     {q:['테스트가 모두 끝났어. 결과 보러 갈래?'],
     a:[{type:'',text:'결과 보러 가기'}]}
@@ -95,10 +96,18 @@ function App() {
   }
  }
 
-
  const setProgress = useCallback((pageidx) => {
   var progress = document.querySelector('.questionProgressBar');
-  progress.style.width = (100/questionList.length) * pageidx + '%';
+  
+  let t = (100/questionList.length) * (pageidx - 1)
+  
+  // progress.style.width = (100/questionList.length) * pageidx + '%';
+  const barAnimation = setInterval(() => {
+    progress.style.width =  t + '%'
+    t++ >= (100/questionList.length) * pageidx && clearInterval(barAnimation)
+  }, 20)
+
+
  }, [questionList])
 
  useEffect(() => {
@@ -176,6 +185,7 @@ function App() {
             <div className='questionProgressBg'>
               <div className='questionProgressBar'>
                 </div>
+                <div className='questionProgressDot'/>
             </div>
           </div>
 
@@ -191,16 +201,19 @@ function App() {
                     </div>
                   )}
                 </div>
-              <div className='answerItemLayout'>
-                {val.a.map((aval,aidx)=>
-                  <div key={aidx} className='answerBox' onClick={()=>handleCkAnswer(aval.type,idx)}>
-                    {aval.text}
-                  </div>
-                )}
-              </div>
             </div>
-
           )}
+
+          {questionList.map((val,idx)=>
+            <div className='answerItemLayout' style={{display:page===idx+1?'flex':'none'}} key={idx}>
+            {val.a.map((aval,aidx)=>
+              <div key={aidx} className='answerBox' onClick={()=>handleCkAnswer(aval.type,idx)}>
+                {aval.text}
+              </div>
+            )}
+          </div>
+          )}
+
         </div>
         :
         <div className='questionLayout'>
